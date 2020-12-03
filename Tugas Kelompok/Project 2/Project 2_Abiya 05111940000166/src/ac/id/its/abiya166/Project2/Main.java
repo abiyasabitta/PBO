c19318ff4a71e6b548c1752cb0cd723a91bb21c9
abiyasabitta <40336175+abiyasabitta@users.noreply.github.com>
2020-12-03 10:07:47 +0700

    Add files via upload

---
+++ Tugas Kelompok/Project 2/Project 2_Abiya 05111940000166/src/ac/id/its/abiya166/Project2/Main.java
@@ -0,0 +1,15 @@
+package ac.id.its.abiya166.Project2;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    LabelFrame label = new LabelFrame();
+	    JFrame app = new JFrame();
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.add(label);
+	    app.setSize(350,  250);
+	    app.setVisible(true);
+    }
+}
